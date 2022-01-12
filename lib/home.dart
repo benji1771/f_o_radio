01e47aec7c10ba90c9bf40b76898c866de6ba416
Benjamin Sarol <sarolbenjamin_08221999@yahoo.com>
2022-01-11 19:54:44 -0600

    forgot to save

--- lib/home.dart
+++ lib/home.dart
@@ -2,7 +2,6 @@
 
 import 'package:f_o_radio/station.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
@@ -16,8 +15,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   Future<List<Station>> _getStationList() async {
     final http.Response response = await http.get(Uri.parse(
         'http://de1.api.radio-browser.info/json/stations/byname/jazz'));
@@ -32,19 +29,12 @@ class _MyHomePageState extends State<MyHomePage> {
   late Future<List<Station>> stationsList;
   @override
   void initState() {
-    // TODO: implement initState
     stationsList = _getStationList();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       backgroundColor: Colors.orange[700],
       body: SafeArea(
